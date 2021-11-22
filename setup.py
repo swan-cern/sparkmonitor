a12b8544dda93e75658dceaa907030dcb9600833
Krishnan R <krishnanr1997@gmail.com>
2021-11-22 09:41:25 +0100

    Fix build issues

--- setup.py
+++ setup.py
@@ -1,34 +1,30 @@
 import json
 from pathlib import Path
-
-from jupyter_packaging import wrap_installers, npm_builder, get_data_files
 from setuptools import find_packages, setup
 
-
 HERE = Path(__file__).parent.resolve()
-
 lab_path = HERE / "sparkmonitor" / "labextension"
-
 ensured_targets = [
     str(lab_path / "package.json"), 
     str(HERE / "sparkmonitor" / "static/extension.js"),
     str(HERE / "sparkmonitor" / "listener_2.11.jar"),
     str(HERE / "sparkmonitor" / "listener_2.12.jar")
     ]
-
 data_file_spec = [
     (f"share/jupyter/labextensions/sparkmonitor", str(lab_path), "**"),
     (f"share/jupyter/labextensions/sparkmonitor", str(HERE), "install.json"),
 ]
-
-
-builder = npm_builder(build_cmd="build:prod", build_dir=lab_path, source_dir="src")
-
-cmdclass = wrap_installers(post_develop=builder, ensured_targets=ensured_targets)
-
 long_description = (HERE / "README.md").read_text()
-
 pkg_json = json.loads((HERE / "package.json").read_bytes())
+
+try:
+    from jupyter_packaging import wrap_installers, npm_builder, get_data_files
+    builder = npm_builder(build_cmd="build:prod", build_dir=lab_path, source_dir="src")
+    cmdclass = wrap_installers(pre_dist=builder, ensured_targets=ensured_targets)
+    data_files=get_data_files(data_file_spec)
+except ImportError: 
+    cmdclass={}
+    data_files=[]
 
 setup(
     name=pkg_json["name"],
@@ -47,10 +43,9 @@ setup(
     license=pkg_json["license"],
     zip_safe=False,
     python_requires=">=3.6",
-    data_files=get_data_files(data_file_spec),
+    data_files=data_files,
     install_requires=[
-        "jupyterlab~=3.0",
-        "jupyter_packaging~=0.9,<2",
+        "jupyterlab~=3.0"
     ],
     platforms="Linux, Mac OS X, Windows",
     keywords=["Jupyter", "JupyterLab", "JupyterLab3"],
